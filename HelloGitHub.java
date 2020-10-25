475fd74a903f315ab763a11e581af28c5c5d1b40
weibgg <weibgg@163.com>
2020-10-25 19:58:07 +0800

    冲突解决并提交

--- HelloGitHub.java
+++ HelloGitHub.java
@@ -3,7 +3,7 @@ package xyz.newtouch;
 public class HelloGitHub {
     public static void main(String[] args) {
         System.err.println("HelloWorld");
-	System.out.println("干的不错");
+	System.out.println("干的不错，666");
     }
 }
 
